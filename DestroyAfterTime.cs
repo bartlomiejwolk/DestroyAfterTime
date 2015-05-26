26b090a52bc9905771bbe43a85d0aa600b68abe2
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-26 12:50:55 +0200

    Initial commit

---
+++ DestroyAfterTime.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace OneDayGame {
+	public class DestroyAfterTime : GameComponent {
+
+		/// GO to be destroyed.
+		[SerializeField]
+			private GameObject _target;
+
+		/// Destroy delay.
+		[SerializeField]
+			private float _delay;
+
+		public override void Start () {
+			base.Start();
+
+			if (_target) {
+				Destroy(_target, _delay);
+			}
+			else {
+				MissingReference("_target");
+			}
+		}
+
+	    public void Now() {
+	        Destroy(_target);
+	    }
+	}
+}
