9828fd6fbeb45921f6ba13dab373076dac91a1fa
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-26 13:42:00 +0200

    Add licence info

--- Utilities.cs
+++ Utilities.cs
@@ -1,3 +1,8 @@
+// Copyright (c) 2015 Bartlomiej Wolk (bartlomiejwolk@gmail.com)
+//  
+// This file is part of the DestroyAfterTime extension for Unity.
+// Licensed under the MIT license. See LICENSE file in the project root folder.
+
 using System;
 using System.Diagnostics;
 using System.Reflection;
